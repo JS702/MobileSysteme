65589380576e84edc960586c962d0e45ffb0c1b5
VinzentPilz <81297495+VinzentPilz@users.noreply.github.com>
2023-05-24 16:17:47 +0200

    Revert "test für jon"
    This reverts commit 4390d1e5d335826baaf11bb2b091286903746257.

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import BaseLayout from "./components/base-layout";
 import Map from "./components/map";
-import Map2 from "./components/map2";
 
 export default function App() {
     return (
-            //<BaseLayout/>
+            <BaseLayout/>
             //<Map/>
-            <Map2/>
     );
 }
 
