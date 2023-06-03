8b8d9a1f27e633ef173d35b32ca69eaf9ef56376
VinzentPilz <81297495+VinzentPilz@users.noreply.github.com>
2023-06-03 19:20:34 +0200

    friends panel update

--- components/base-layout.js
+++ components/base-layout.js
@@ -1,12 +1,26 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, Button } from "react-native";
 import FriendPanel from "./friend-panel";
 import MapWebview from "./map/map-webview";
+import { FAB } from "@rneui/themed";
 
 const BaseLayout = () => {
+
+    const [ showSidePanel, setShowSidePanel ] = useState( false );
+
+    const togglePanel = () => {
+        setShowSidePanel( !showSidePanel );
+    };
+
     return (
             <SafeAreaView style={ styles.baseLayout }>
-                <FriendPanel style={ styles.panel }/>
-                
+                <FAB //Friends Button
+                    style={styles.friendsButton}
+                    icon={{ type:"fontisto", name: "persons", color: 'white' }}
+                    color="green"
+                    onPress={ togglePanel }
+                />
+                { showSidePanel && <FriendPanel style={ styles.panel }/> }                
                 <MapWebview/>
                 
             </SafeAreaView>
@@ -25,7 +39,17 @@ const styles = StyleSheet.create( {
         height: "100%",
         //width: "100%",
 
-    }
+    },
+    friendsButton: {
+        position:'absolute',
+        right:0,
+        top: 0,
+        marginRight:10,
+        marginTop:10,
+        height:50,
+        width:50,
+        zIndex: 1
+    },
 } );
 
 export default BaseLayout;
