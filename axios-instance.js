2c1f14dfe0f9044392e2d926acf078722bcb8868
Jon Steinkamp <steinkamp@moovin.de>
2023-06-10 15:09:18 +0200

    removed headers

--- axios-instance.js
+++ axios-instance.js
@@ -1,15 +1,6 @@
 import axios from "axios";
 
-const headers = () => { // KP ob Stefan header benutzt
-    if ( typeof window !== "undefined" ) {
-        return localStorage.getItem( "jwtToken" )
-                ? { Authorization: "Bearer " + localStorage.getItem( "jwtToken" ) }
-                : null;
-    }
-};
-
 export default axios.create( {
     //baseURL: "https://amaranth-monkey-tam.cyclic.app/api",
     baseURL: "http://[HOSTNAME]:5000/api"
-    //headers: headers()
 } );
