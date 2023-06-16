c9f9f9fe644ac6acef1a4b4b17d248d9e0bcf326
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-06-16 20:40:30 +0200

    test

--- components/compass.js
+++ components/compass.js
@@ -1,110 +1,107 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Magnetometer } from "expo-sensors";
-import { Icon } from "@rneui/themed";
+import Icon from "react-native-vector-icons/Entypo";
 
+Icon.loadFont();
 
-const Compass = ( { angle, ownLocation, friendLocation, distance } ) => {
+const Compass = ({ angle, ownLocation, friendLocation, distance }) => {
+  const [subscription, setSubscription] = useState(null);
+  const [magnetometer, setMagnetometer] = useState(0);
 
-    const [ subscription, setSubscription ] = useState( null );
-    const [ magnetometer, setMagnetometer ] = useState( 0 );
-
-
-    useEffect( () => {
-        _toggle();
-        return () => {
-            _unsubscribe();
-        };
-    }, [] );
-
-    const caculauteDir = () => {
-        const vector = [ friendLocation.lng - ownLocation.lng, friendLocation.lat - ownLocation.lat ];
-        const theta = angle * 180 / Math.PI;
-
-        const cs = Math.cos( theta );
-        const sn = Math.sin( theta );
-
-        const x = ( ownLocation.lat + 1 ) * cs - ownLocation.lng * sn;
-        const y = ( ownLocation.lat + 1 ) * sn + ownLocation.lng * cs;
-
-        const otherVector = [ x, y ];
-        return Math.atan2(  vector[ 0 ] - otherVector[ 0 ], vector[ 1 ] - otherVector[ 1 ] ) * 180 / Math.PI;
+  useEffect(() => {
+    _toggle();
+    return () => {
+      _unsubscribe();
     };
+  }, []);
 
-    const _toggle = () => {
-        if ( subscription ) {
-            _unsubscribe();
-        } else {
-            _subscribe();
-        }
-    };
+  const calculateDir = () => {
+    const vector = [
+      friendLocation.lng - ownLocation.lng,
+      friendLocation.lat - ownLocation.lat,
+    ];
+    const theta = Math.atan2(vector[1], vector[0]);
+    return theta * (180 / Math.PI);
+  };
 
-    const _subscribe = () => {
-        setSubscription(
-                Magnetometer.addListener( ( data ) => {
-                    setMagnetometer( _angle( data ) );
-                } )
-        );
-    };
+  const _toggle = () => {
+    if (subscription) {
+      _unsubscribe();
+    } else {
+      _subscribe();
+    }
+  };
 
-    const _unsubscribe = () => {
-        subscription && subscription.remove();
-        setSubscription( null );
-    };
-
-    const _angle = ( magnetometer ) => {
-        let angle = 0;
-        if ( magnetometer ) {
-            let { x, y, z } = magnetometer;
-            if ( Math.atan2( y, x ) >= 0 ) {
-                angle = Math.atan2( y, x ) * 180 / Math.PI;
-            } else {
-                angle = Math.atan2( y, x ) * 180 / Math.PI;
-            }
-        }
-        return Math.round( angle );
-    };
-
-
-    return (
-        <View style={ { backgroundColor:  "#00000088",
-                        borderRadius: 90,
-                        borderTopLeftRadius: 0,
-                        borderTopRightRadius: 0,
-                        width: 60,
-                        height: 80,
-                        justifyContent: "flex-end",
-                        alignItems: "center"
-                    } }>
-            <Text style={ {
-                        color: "white",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        fontSize: 11
-                    } }>{ distance }</Text>
-            <View style={ { //backgroundColor:  "#ff000088",
-                            borderRadius: 90,
-                            //marginTop: 8,
-                            width: 60,
-                            height: 60,
-                            justifyContent: "center",
-                            alignItems: "center"
-                        } }>
-
-                <Icon
-                    type="entypo"
-                    name="direction"
-                    color="white"
-                    size={30}
-                    style={ {
-                        resizeMode: "contain",
-                        transform: "".concat( "rotate(", 45 - 360 - magnetometer + caculauteDir(), "deg)" )
-                    } }   
-                />
-            </View>
-        </View>
+  const _subscribe = () => {
+    setSubscription(
+      Magnetometer.addListener((data) => {
+        setMagnetometer(_angle(data));
+      })
     );
-};
+  };
 
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const _angle = (magnetometer) => {
+    let angle = 0;
+    if (magnetometer) {
+      let { x, y, z } = magnetometer;
+      angle = Math.atan2(y, x) * (180 / Math.PI);
+      if (angle < 0) {
+        angle += 360;
+      }
+    }
+    return Math.round(angle);
+  };
+
+  return (
+    <View
+      style={{
+        backgroundColor: "#00000088",
+        borderRadius: 90,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        width: 60,
+        height: 80,
+        justifyContent: "flex-end",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "white",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: 11,
+        }}
+      >
+        {distance}
+      </Text>
+      <View
+        style={{
+          borderRadius: 90,
+          width: 60,
+          height: 60,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Icon
+          name="direction"
+          color="white"
+          size={30}
+          style={{
+            resizeMode: "contain",
+            transform: `rotate(${45 - 360 - magnetometer + calculateDir()}deg)`,
+          }}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default Compass;
